957a8d00157186ffb42d9c26e6b07388cbee8933
migueldiezvi <migueldiezvi@gmail.com>
2019-02-02 12:34:12 +0100

    automático

--- sube.sh
+++ sube.sh
@@ -1,3 +1,9 @@
-git add .
-git commit -m "automático"
-git push origin master
+if [ $# == 0 ]; then
+    echo "UpToGit: ¡Error! No se le ha pasado ningún parámetro"
+    echo "uptogit fichero1 fichero2 ... ficheroN"
+    exit -1
+else
+    git add $*
+    git commit -m "automático"
+    git push origin master
+fi
